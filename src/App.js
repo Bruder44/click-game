669e941ba74db57016a925a2ce076ab827521c30
Matthew Bruder <mbruder@students.kennesaw.edu>
2018-06-28 18:08:50 -0400

    update

---
+++ src/App.js
@@ -0,0 +1,126 @@
+import React, { Component } from "react";
+import MatchCard from "./components/MatchCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import matches from "./matchcards.json";
+import "./App.css";
+
+let correctGuesses = 0;
+let bestScore = 0;
+let clickMessage = "Click on a player to earn a point, but don't click on any of them more than once! Get 12 in a row to win!";
+
+class App extends Component {
+    
+    // Setting this.state.matches to the matches json array
+    state = {
+        matches,
+        correctGuesses,
+        bestScore,
+        clickMessage
+    };
+
+    setClicked = id => {
+
+        // Make a copy of the state matches array to work with
+        const matches = this.state.matches;
+
+        // Filter for the clicked match
+        const clickedMatch = matches.filter(match => match.id === id);
+
+        // If the matched image's clicked value is already true, end game with clickmessage
+        if (clickedMatch[0].clicked){
+
+            console.log ("Correct Guesses: " + correctGuesses);
+            console.log ("Best Score: " + bestScore);
+
+            correctGuesses = 0;
+            clickMessage = "Oops! You've already clicked on that player! Start over!"
+
+            for (let i = 0 ; i < matches.length ; i++){
+                matches[i].clicked = false;
+            }
+
+            this.setState({clickMessage});
+            this.setState({ correctGuesses });
+            this.setState({matches});
+
+        // Otherwise, if clicked = false, and the user hasn't finished
+        } else if (correctGuesses < 11) {
+
+            // Set its value to true
+            clickedMatch[0].clicked = true;
+
+            // increment the appropriate counter
+            correctGuesses++;
+            
+            clickMessage = "Correct! Keep going!";
+
+            if (correctGuesses > bestScore){
+                bestScore = correctGuesses;
+                this.setState({ bestScore });
+            }
+
+            // Shuffles MatchCards array to be rendered in a random order
+            matches.sort(function(a, b){return 0.5 - Math.random()});
+
+            // Set this.state.matches equal to the new array
+            this.setState({ matches });
+            this.setState({correctGuesses});
+            this.setState({clickMessage});
+        } else {
+
+            // Set the clickedMatch value to true
+            clickedMatch[0].clicked = true;
+
+            // restarting the guess counter
+            correctGuesses = 0;
+
+            // Victory message, reset game
+            clickMessage = "Congratulations!!! You got them all without a mistake!!! Can you do it again?";
+            bestScore = 12;
+            this.setState({ bestScore });
+            
+            for (let i = 0 ; i < matches.length ; i++){
+                matches[i].clicked = false;
+            }
+
+            // Shuffle the array to be rendered in a random order
+            matches.sort(function(a, b){return 0.5 - Math.random()});
+
+            // Set this.state.matches equal to the new array
+            this.setState({ matches });
+            this.setState({correctGuesses});
+            this.setState({clickMessage});
+
+        }
+    };
+
+    render() {
+        return (
+            <Wrapper>
+                <Title>Click through the English Players at the World Cup!!</Title>
+        
+                <h3 className="scoreSummary">
+                    {this.state.clickMessage}
+                </h3>
+                
+                <h3 className="scoreSummary">
+                    Correct Guesses: {this.state.correctGuesses} 
+                    <br />
+                    Best Score: {this.state.bestScore} 
+                </h3>
+
+                {this.state.matches.map(match => (
+                    <MatchCard
+                        setClicked={this.setClicked}
+                        id={match.id}
+                        key={match.id}
+                        image={match.image}
+                    />
+                ))}
+            </Wrapper>
+        );
+    }
+}
+
+export default App;
